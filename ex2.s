6996086f8be58ebedc34f05f525f8aee7ea2579f
pi <pi>
2018-12-19 11:25:42 +0100

    EX2.S CAPS LOCK

--- ex2.s
+++ ex2.s
@@ -1,15 +1,43 @@
-.data:
-	x:.word
-	y:.word
-.global sqr
-sqr:
+.data
+	x:.word 5
+	y:.word 100
+.text
+.global main
+
+main:
 	ldr r2, =x
 	ldr r0, [r2]
+	ldr r2, =y
 	ldr r1, [r2]
-	mul r2, r0, r1
-	str r2, =x
+	bl sum_sqr
+	bl exit
+
+sqr: // r0 a la valeur donc on renvoit le carre (modifie r0 a r3)
+	mov r1, r0
+	mul r0, r1, r0
 	bx lr
 
-.global sum_sqr
-sum_sqr:
-	
+sum_sqr: // r0 et r1 ont les valeurs dont on veut la somme des carrés (r0 a r3)
+	sub sp, sp, #8
+	str lr, [sp]
+
+	sub sp, sp, #8
+	str r1, [sp] // sauve r1 dans sp
+
+	bl sqr // appelle sqr sur r0
+
+	ldr r1, [sp]
+	str r0, [sp]
+
+	mov r0, r1
+
+	bl sqr // appelle sqr sur la deuxieme valeur
+
+	ldr r1, [sp]
+	add sp, sp, #8
+
+	add r0, r0, r1
+
+	ldr lr, [sp]
+	add sp, sp, #8
+	bx lr
