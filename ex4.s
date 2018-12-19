2f077c9b4614759484a295f3a657da4f6b81c9a0
pi <pi>
2018-12-19 13:01:52 +0100

    Commit

--- ex4.s
+++ ex4.s
@@ -1,17 +1,32 @@
 .data
-	 n .word 10
+	n: .word 10
+        print:.asciz "%d"
+        msg1:.asciz "Entrez un entier N:"
+        aff1:.asciz "fib(n) vaut %d \n"
+
 
 .text
 .global main
 main:
+        ldr r0, =msg1
+        bl printf// demande un entier
+        ldr r0, =print
+        ldr r1, =n
+        bl scanf // entre un entier
+
 	ldr r1, =n
 	ldr r0, [r1]
 	bl fib0
+
+        mov r1, r0
+        ldr r0, =aff1
+        bl printf
+
 	bl exit
 fib0:
 	cmp r0, #1
 	bgt fib
-	move r0, #1
+	mov r0, #1
 	bx lr
 
 fib:
@@ -31,6 +46,6 @@ depil:
 	bx lr
 
 fin:
-	str r1, #1
-	str r2, #1
+	mov r1, #1
+	mov r2, #1
 	b depil
