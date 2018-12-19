b90494fdeb82bddf514c52c7bb53a8460819fc11
pi <pi>
2018-12-19 10:21:38 +0100

    Commit

--- ex1.s
+++ ex1.s
@@ -1,7 +1,11 @@
 .data 
-	a1:.word
-	a2:.word
-.global permute
+	a1:.word 1
+	a2:.word 2
+
+.global main
+main:
+	bl permute
+
 permute:
 	sub sp, sp, #8
 	str lr, [sp]
@@ -14,10 +18,10 @@ permute:
 
 	ldr r2, =a2
 	ldr r0, [r2]
-	str r0, =a1
+	str r0, a1
 
 	ldr r1, [sp]
-	str r1, =a2
+	str r1, a2
 
 	add sp, sp, #8
 
