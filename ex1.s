5ebc0ab015cf6b26063188ca964b78005a0f2d94
pi <pi>
2018-12-19 10:36:55 +0100

    ex1.s fait

--- ex1.s
+++ ex1.s
@@ -1,10 +1,13 @@
 .data 
-	a1:.word 1
-	a2:.word 2
-
+.balign 4
+	a1: .word 1
+.balign 4
+	a2: .word 2
+.text
 .global main
 main:
 	bl permute
+	bl exit
 
 permute:
 	sub sp, sp, #8
@@ -14,14 +17,15 @@ permute:
 
 	ldr r2, =a1
 	ldr r1, [r2]
-	str r1, [sp]
+	str r1, [sp] // on stocke la valeur en a1 dans la pile
 
 	ldr r2, =a2
 	ldr r0, [r2]
-	str r0, a1
+	ldr r1, =a1
+	str r0, [r1] // On stocke la valeur de a1 a l'adresse de a2
 
 	ldr r1, [sp]
-	str r1, a2
+	str r1, [r2] // On stocke la valeur de la pile en r2
 
 	add sp, sp, #8
 
@@ -29,4 +33,4 @@ permute:
 	ldr lr, [sp]
 	add sp, sp, #8
 
-	bl exit
+	bx lr
