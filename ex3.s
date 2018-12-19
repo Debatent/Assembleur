54ae41465b9daeeeb256e1da285958accae1f2c5
pi <pi>
2018-12-19 11:03:05 +0100

    final ex3.s

--- ex3.s
+++ ex3.s
@@ -1,28 +1,37 @@
+.data
+	n:.word 10
 
+.text
+.global main
+main:
 	ldr r1, =n
 	ldr r2, [r1]
 	mov r0, r2
+	bl sumI
+	bl exit
 
+sumI: // quand on appelle sum depuis le main, r0 = r2 = nbr dont on veut la somme inferieure
 	sub  sp, sp, #8
 	str lr, [sp]
+	b sum
 sum:
 	cmp r0, #0
 	ble sum0
 	sub sp, sp, #8
 	str r0, [sp]
 	sub r0, #1
-	bl sum0
+	b sum
 depil:
-	add sp, sp, #8
 	ldr r0, [sp]
+	add sp, sp, #8
 	add r1, r1, r0
 	cmp r2, r0
 	bgt depil
-	bl endfunc
+	b endfunc
 sum0:
 	sub sp, sp, #8
         str r0, [sp]
-	ldr r1, #0
+	mov r1, #0 //r1 va etre la somme
 	cmp r2, r0
 	bgt depil
 endfunc:
@@ -30,4 +39,3 @@ endfunc:
 	ldr lr, [sp]
 	add sp, sp, #8
 	bx lr
-
